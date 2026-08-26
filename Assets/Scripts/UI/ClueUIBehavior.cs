4673adea5c00255fa6ad04ce2afae39c3725c3cb
Dylan33431 <33431@ma-web.nl>
2026-08-26 12:55:46 +0200

    Ui Updater

---
+++ Assets/Scripts/UI/ClueUIBehavior.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClueUIBehavior : MonoBehaviour
+{
+    private CanvasGroup ClueUIGroup;
+    private TMP_Text ClueText;
+    public string ClueInfo;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ClueUIGroup = GetComponent<CanvasGroup>();
+        ClueText = GetComponentInChildren<TMP_Text>();
+        ActivateUI();
+    }
+    void ActivateUI()
+    {
+        UpdateUI();
+        ClueUIGroup.alpha = 1;
+    }
+    void DeactivateUI()
+    {
+        ClueUIGroup.alpha = 0;
+    }
+    void UpdateUI()
+    {
+        Debug.Log(ClueInfo);
+        ClueText.text = ClueInfo;
+    }
+}
